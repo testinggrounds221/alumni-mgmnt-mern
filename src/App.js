6f472a4b499063177e4424b979da5a8d8ec94ed8
RamKri <testinggrounds221@gmail.com>
2021-04-12 20:24:03 +0530

    Init Deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Trying Rebuild
         </p>
         <a
           className="App-link"
