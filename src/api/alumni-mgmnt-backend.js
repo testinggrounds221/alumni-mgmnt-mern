aa5a38415a374da583ccd0ecf05c680fb12e5a80
RamKri <testinggrounds221@gmail.com>
2021-04-18 21:48:57 +0530

    url change

--- src/api/alumni-mgmnt-backend.js
+++ src/api/alumni-mgmnt-backend.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const dev = "http://localhost:3000";
-//const prod = "https://alumni-mgmnt-backend.herokuapp.com";
+//const dev = "http://localhost:3000";
+const prod = "https://alumni-mgmnt-backend.herokuapp.com";
 
 export default axios.create({
-  baseURL: dev,
+  baseURL: prod,
   // headers: {
   //   Authorization:
   //     "Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296",
